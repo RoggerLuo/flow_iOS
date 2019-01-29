3236243abc7b38764b1466d94d326f590770cd7e
Roger <luojie.5408@163.com>
2019-01-29 13:31:18 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,123 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'dart:async';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-		title: 'Startup Name Generator',            
-		home: new MyGetHttpData(),
+    return new MaterialApp(
+      title: 'Infinite List',
+      theme: new ThemeData(
+          primaryColor: Colors.blue, accentColor: Colors.lightBlue),
+      home: new RandomWords(),
     );
   }
 }
-// Create a stateful widget
-class MyGetHttpData extends StatefulWidget {
-  @override
-  MyGetHttpDataState createState() => new MyGetHttpDataState();
-}
-// Create the state for our stateful widget
 
-class MyGetHttpDataState extends State<MyGetHttpData> {
-  final String url = "http://rorrc.3322.org:6664/notes";
-  List data;
-  
-  Future<String> getJSONData() async {
-    var response = await http.get(
-        Uri.encodeFull(url),
-        headers: {"Accept": "application/json"}
-	);
-    print(response.body);
-    setState(() {
-		var dataConvertedToJSON = json.decode(response.body);
-		data = dataConvertedToJSON['data'];
-    });
-    return "Successfull";
-  }
-  
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Data Demo"),
+        title: new Text('Infinite List'),
+        centerTitle: true,
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
-      // Create a Listview and load the data when available
-      body: new ListView.builder(
-          itemCount: data == null ? 0 : data.length,
-          itemBuilder: (BuildContext context, int index) {
-            return new Container(
-              child: new Center(
-                  child: new Column(
-                // Stretch the cards in horizontal axis
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  new Card(
-                    child: new Container(
-                      child: new Text(
-                        // Read the name field value and set it in the Text widget
-                        data[index]['content'],
-                        // set some style to text
-                        style: new TextStyle(
-                            fontSize: 20.0, color: Colors.lightBlueAccent),
-                      ),
-                      // added padding
-                      padding: const EdgeInsets.all(15.0),
-                    ),
-                  )
-                ],
-              )),
-            );
-          }),
+      body: _buildSuggestions(),
     );
   }
-   @override
-  void initState() {
-    super.initState();
 
-    // Call the getJSONData() method when the app initializes
-    this.getJSONData();
-  }
-}
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile
+              .divideTiles(
+                context: context,
+                tiles: tiles,
+              )
+              .toList();
 
-class RandomWordsState extends State<RandomWords> {
-	final _suggestions = <WordPair>[];
-  	final _biggerFont = const TextStyle(fontSize: 18.0);
-	Widget _buildSuggestions() {
-		return ListView.builder(
-			padding: const EdgeInsets.all(16.0),
-			itemBuilder: /*1*/ (context, i) {
-				if (i.isOdd) return Divider(); /*2*/
-
-				final index = i ~/ 2; /*3*/
-				if (index >= _suggestions.length) {
-				_suggestions.addAll(generateWordPairs().take(10)); /*4*/
-				}
-				return _buildRow(_suggestions[index]);
-			}
-		);
-	}
-	Widget _buildRow(WordPair pair) {
-		return ListTile(
-			title: Text(
-			pair.asPascalCase,
-			style: _biggerFont,
-			),
-		);
-	}
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-			title: Text('Startup Name Generator'),
-			),
-			body: _buildSuggestions(),
-		);
-	}
-}
-
-class RandomWords extends StatefulWidget {
-	@override
-	RandomWordsState createState() => new RandomWordsState();
-}
-
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text('Saved lists'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+            body: new ListView(children: divided),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(0),
+        // The itemBuilder callback is called once per suggested word pairing,
+        // and places each suggestion into a ListTile row.
+        // For even rows, the function adds a ListTile row for the word pairing.
+        // For odd rows, the function adds a Divider widget to visually
+        // separate the entries. Note that the divider may be difficult
+        // to see on smaller devices.
+        itemBuilder: (context, i) {
+          // Add a one-pixel-high divider widget before each row in theListView.
+          // if (i.isOdd) return new Divider();
+
+          // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+          // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+          // This calculates the actual number of word pairings in the ListView,
+          // minus the divider widgets.
+          final index = i ; //~/ 2;
+          // If you've reached the end of the available word pairings...
+          if (index >= _suggestions.length) {
+            // ...then generate 10 more and add them to the suggestions list.
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
   }
 }
