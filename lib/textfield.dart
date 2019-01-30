0ddcfb5f096004fd097bb81d2788b05ccd5db69b
Roger <luojie.5408@163.com>
2019-01-30 15:07:25 +0800

    Update

--- lib/textfield.dart
+++ lib/textfield.dart
@@ -128,31 +128,3 @@ class RandomWordsState extends State<RandomWords> {
         });
   }
 }
-class AddEntryDialog extends StatefulWidget {
-  @override
-  AddEntryDialogState createState() => new AddEntryDialogState();
-}
-
-class AddEntryDialogState extends State<AddEntryDialog> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: const Text('New entry'),
-        actions: [
-          new FlatButton(
-              onPressed: () {
-                //TODO: Handle save
-              },
-              child: new Text('SAVE',
-                  style: Theme
-                      .of(context)
-                      .textTheme
-                      .subhead
-                      .copyWith(color: Colors.white))),
-        ],
-      ),
-      body: new Text("Foo"),
-    );
-  }
-}
