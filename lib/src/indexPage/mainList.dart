f32c625a79fac320bcff25ff243fbf3f74c814f5
Roger <luojie.5408@163.com>
2019-03-07 15:45:33 +0800

    Update

--- lib/src/indexPage/mainList.dart
+++ lib/src/indexPage/mainList.dart
@@ -35,39 +35,6 @@ Widget getSelectableTags(List tagList,context){
     textOverflow:TextOverflow.fade,
     onPressed: (tag){
       ScopedModel.of<IndexModel>(context, rebuildOnChange: true).tapTag(tag.title);
-
-      // setState(() {
-      //   _filtered_notes = [];
-      // });
-      // String tagText = tag.title;
-      // if(_selected_tags.indexOf(tagText) == -1){
-      //   _selected_tags.add(tagText);
-      // }else{
-      //   _selected_tags.remove(tagText);
-      // }
-      // if(_selected_tags.length == 0) {
-      //   setState(() {
-      //     _filtered_notes = _notes;
-      //   });
-      //   return;
-      // }
-      // for (var x = 0; x < _notes.length; x++) {
-      //   Note _note = _notes[x];
-      //   bool thisNoteIncludesAllKeywords = true;
-      //   bool thisNoteIsNotCurrentNote = true;
-      //   for (var j = 0; j < _selected_tags.length; j++) {
-      //     var currentWord = _selected_tags[j];
-      //     if(_note.matchList.indexOf(currentWord) == -1){
-      //       thisNoteIncludesAllKeywords = false;
-      //     }
-      //     if(_note.id == widget.note.id){ // 去除当前笔记
-      //       thisNoteIsNotCurrentNote = false;            
-      //     }
-      //   }
-      //   if(thisNoteIncludesAllKeywords && thisNoteIsNotCurrentNote) {
-      //     _filtered_notes.add(_note);
-      //   }
-      // }
     },
   );
 }
@@ -96,28 +63,13 @@ var buildMainList = () => ScopedModelDescendant<IndexModel>(
         int noteIndex = index - 1; // 因为第一位被占了 但是+1了
         int lastIndexInTotal = model.notes.length + 2 - 1;
         if(index != lastIndexInTotal) {
-          return buildNoteRow(model.notes[noteIndex],context,index);
+          return buildNoteRow(model.notes[noteIndex],context,noteIndex);
         }
         if(index == lastIndexInTotal) {
           int noteCount = noteIndex+1;
           if(noteCount<20) return null; // 小于一次分页的量也别显示，不用分页了就
           return buildProgressIndicator(model.isPerformingRequest);          
         }
-        // if (index == ((model.notes.length-1)+2) ) {
-        //   if(index == 0 ) return null;
-        //   if(index > ((20-1) +2 ) ) {
-        //     return buildProgressIndicator(model.isPerformingRequest);          
-        //   }else{
-        //     return null;
-        //   }
-        // } else {
-        //   print(index-1);
-        //   if(model.notes.length!=0){
-        //     return buildNoteRow(model.notes[index-1],context,index);
-        //   }else{
-        //     return null;
-        //   }
-        // }
       }
     )
   )
